d9b9b874c4bd171fba0031e2ef80554741bd3622
AndrewMatsiash <rabotatnadomne@gmail.com>
2022-05-02 22:52:45 +0300

    test carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!sampleActivity || (typeof sampleActivity) !== 'string' || isNaN(sampleActivity) || +sampleActivity < 1) {
+    return false
+  }
+  let year = 0
+  let per = 0
+  per = 0.693 / HALF_LIFE_PERIOD
+  year = (Math.log(MODERN_ACTIVITY / sampleActivity)) / per
+
+  if (year < 0) return false
+
+  return Math.trunc(Math.ceil(year))
 }
 
 module.exports = {
